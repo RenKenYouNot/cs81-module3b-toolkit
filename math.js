80e894151e04113445d2fa8eed6e8cb7825c9a55
RenKenYouNot <airenalires@gmail.com>
2025-07-05 18:24:44 -0700

    isEven(n) math.js

--- math.js
+++ math.js
@@ -1,3 +1,4 @@
+// n = 5 for all of the functions
 // function to double number
 function double(n) {
   return n * 2;
@@ -6,4 +7,9 @@ function double(n) {
 function square(n) {
   return n * n;
 }
+// Function to check if value is even
+function isEven(n) {
+  return n % 2 === 0; // This checks if the number 'n' is even
+}
+
 
